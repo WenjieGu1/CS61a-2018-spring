7b3426cb36da791908f00ce76fab96c88b7ca077
WenjieGu1 <129292139+WenjieGu1@users.noreply.github.com>
2023-03-31 00:22:09 +0800

    Create lab06_extra.py

---
+++ lab/lab06/lab06_extra.py
@@ -0,0 +1,31 @@
+""" Optional problems for Lab 6 """
+
+## Nonlocal practice
+def vending_machine(snacks):
+    """Cycles through sequence of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    "*** YOUR CODE HERE ***"
+    i=0
+    def vender():
+        nonlocal i
+        if i ==len(snacks):
+            i=0
+        sna=snacks[i]
+        i+=1
+        return sna
+    return vender
